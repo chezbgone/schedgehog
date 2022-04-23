e3062009296d6e535a6c6094684a5af609901cbd
Matthew Ho <homattieho@gmail.com>
2022-04-23 16:50:42 -0400

    Add character and string generators

--- src/generator.scm
+++ src/generator.scm
@@ -23,3 +23,31 @@
   (make-generator (lambda () (= 0 (random 2)))))
 (add-generator 'boolean arbitrary-boolean)
 
+;; Generates a random character of type 'alpha, 'numeric, 'alphanumeric, or just any character. Alphanumeric characters are letters with probability 50% and numbers with probability 50%.
+(define (arbitrary-character #!optional character-type)
+  (assert (or (member character-type (list 'alpha
+                                           'numeric
+                                           'alphanumeric))
+              (default-object? character-type)))
+  (cond
+   ((equal? character-type 'alpha)
+    (let ((char-number (random 52)))
+      (if (> char-number 25)
+          (integer->char (+ 39 char-number))
+          (integer->char (+ 97 char-number)))))
+   ((equal? character-type 'numeric)
+    (integer->char (+ (random 10) 48)))
+   ((equal? character-type 'alphanumeric)
+    (if (= (random 2) 0)
+        (arbitrary-character 'alpha)
+        (arbitrary-character 'numeric)))
+   (else
+    (integer->char (random char-code-limit)))))
+
+;; Generates a random string of a fixed length with characters of type character-type. Character-type must be specified, otherwise printing out characters might result in strings longer than intended.
+(define (arbitrary-string length character-type)
+  (if (<= length 1)
+      (char->name (arbitrary-character character-type))
+      (string-append (char->name (arbitrary-character character-type))
+                 (arbitrary-string (- length 1) character-type))))
+
