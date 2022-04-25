3c6d965e94d504137c49c3cd9ae7c52b9c624348
Jason Chen <chezbgone@gmail.com>
2022-04-25 12:35:35 -0400

    Write some tree utilities

--- src/tree.scm
+++ src/tree.scm
@@ -8,15 +8,34 @@
   (value lazy-tree-value)
   (children lazy-tree-children))
 
+;;; tree utils
+
+(define ((%lazy-tree:map f) tr)
+  (let* ((value (lazy-tree-value tr))       ; a
+         (children (lazy-tree-children tr)) ; list (() -> lazy-tree a)
+         (map-child                         ; (() -> lazy-tree a) -> (() -> lazy-tree b)
+          (lambda (lt) (lambda ()
+            ((%lazy-tree:map f) (lt)))))
+         (mapped-children (map map-child children))) ; list (() -> lazy-tree b)
+    (make-lazy-tree (f value)
+                    mapped-children)))
+
+;; (a -> b) -> (lazy-tree a -> lazy-tree b)
+(define (lazy-tree:map f tr)
+  ((%lazy-tree:map f) tr))
+
+
 (define-record-type tree
   (make-tree value children)
   tree?
   (value tree-value)
   (children tree-children))
 
+;; lazy-tree a -> tree a
 (define (force-tree tr)
   (make-tree (lazy-tree-value tr)
-             (map force-lazy-tree (lazy-tree-children tr))))
+             (map (lambda (subtree) (force-tree (subtree)))
+                  (lazy-tree-children tr))))
 
 (define (print-tree tree)
   (define (%print-tree prefix is-last t)
